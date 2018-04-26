499db146c1aa6ce6264b6b7264a8d92f1ed1e9a3
Connor VanMeter <connor.vanmeter@uky.edu>
2018-04-26 18:30:48 -0400

    Added skeleton to server.c

--- server.c
+++ server.c
@@ -1,3 +1,44 @@
+/*
+ * Description: This is the server for the project.
+ * Course Name: CS371 Project 2
+ * Authors: Tyler Price & Connor VanMeter
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+/* This function handles all communication with clients */
+void *handler_client(void *arg) {
+
+}
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in serv_addr, cli_addr;
+    pthread_t td;
+
+    /* Socket settings */
+
+    /* Bind */
+
+    /* Listen */
+
+    /* Accept clients */
+    while(1) {
+        /* Client settings */
+        client_t *new_client = (client_t)malloc(sizeof(client_t));
+
+        /* Add client to the queue */
+
+        /* Create a thread */
+        pthread_create(&td, NULL, &handle_client, (void*)new_client);
+    }
+}
 
 struct {
 	struct User * array;
