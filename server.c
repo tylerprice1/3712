54eaeb83c12e00195352ff7e9fb2172d40018fdb
Connor VanMeter <connor.vanmeter@uky.edu>
2018-04-27 12:20:42 -0400

    Chat updates to server

--- server.c
+++ server.c
@@ -12,6 +12,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
+#include "Chat.h"
+#include "csapp.h"
 
 /* This function handles all communication with clients */
 void *handler_client(void *arg) {
@@ -19,8 +21,20 @@ void *handler_client(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-    struct sockaddr_in serv_addr, cli_addr;
+    int listenfd, new_socket, port;
+    socklen_t clientlen;
+    struct sockaddr_in serv_add, cli_addr;
+    struct hostent * hp;
+    char * haddrp;
     pthread_t td;
+
+    if (argc != 2) {
+        printf("Wrong arg count");
+        return;
+    }
+
+    struct Chat chat;
+    struct Message received, toSend;
 
     /* Socket settings */
     /* domain = AF_INET IPv4; AF_INET6 IPv6 */
@@ -46,7 +60,7 @@ int main(int argc, char *argv[]) {
 
     /* Accept clients */
     addrLen = sizeof(cli_addr);
-    int new_socket = accept(sockid, &cli_addr, addrLen);
+    new_socket = accept(sockid, &cli_addr, addrLen);
 
     while(1) {
         /* Client settings */
@@ -58,18 +72,28 @@ int main(int argc, char *argv[]) {
             /* unsuccesful */
             return;
         }
+        /* Check hp */
+        haddrp = inet_ntoa(clientaddr.sin_addr); //Check names
+        printf("Server connected to %s (%s)\n", hp->h_name, haddrp);
+        Chat_init(&chat, new_socket, NULL, NULL);
+        Chat_receive(&chat, &received);
+        while (received.type != EXIT_REQUEST) {
+            if (received.type == NEW_MESSAGE) {
+                print("%s: %s\n", received.username, received.text);
+                Message_initAndSet(&toSend, NEW_MESSAGE, "<server received>", received.text);
+                Chat_send(&chat, &toSend);
+            }
+            Chat_receive(&chat, &received);
+        }
+        printf("Server disconnected from %s (%s)\n", hp->h_name, haddrp);
+        close(new_socket); 
 
+
+        // Check if these two are needed
         send();
         receive();
-        close();
 
         /* Create a thread */
-        pthread_create(&td, NULL, &handle_client, (void*)new_client);
+        pthread_create(&td, NULL, &handler_client, (void*)new_client);
     }
 }
-
-struct {
-	struct User * array;
-	unsigned int capacity;
-	unsigned int size;
-} users;
